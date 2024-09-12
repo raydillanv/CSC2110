5bb7a89b370ac74ec3fbad212394cfcd367322f6
Dillan Victory <raydillanv1@gmail.com>
2024-09-11 20:14:13 -0500

    t1 fix

--- t1sd.s
+++ t1sd.s
@@ -1,4 +1,4 @@
-/* CSC 2110, Computer Organization
+/* CSC 2110, Computer Organization 
  * Tutorial 1: Introduction to Assembly
  * by William H. Hooper
  *
@@ -7,14 +7,18 @@
  * degrees Fahrenheit and returns the
  * equivalent degrees Centigrade
  */
-.text
-    .align 4
-    .global remain
-    .type remain,
-    % function
+  .text
+  .align  4
+  .global remain
+  .type remain, %function
 
-            remain : sdiv x2,
-    x0, x1 mul x3, x2, x1 sub x0, x0, x3 ret
-
-                                          .size remain,
-    .- remain.ident "GCC: (SUSE Linux) 4.8.5".ident "William H. Hooper, CSC 1110, t1".section.note.GNU - stack, "", % progbits
+remain:
+  sdiv  x2, x0, x1
+  mul   x3, x2, x1
+  sub   x0, x0, x3
+  ret
+  
+  .size remain, .-remain
+  .ident  "GCC: (SUSE Linux) 4.8.5"
+  .ident  "William H. Hooper, CSC 1110, t1"
+  .section  .note.GNU-stack,"",%progbits
